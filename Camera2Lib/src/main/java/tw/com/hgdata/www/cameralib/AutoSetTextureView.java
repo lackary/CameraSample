390ad5d650f6d34628f873f4c20aca059edbce28
lackary <lackar@gmail.com>
2016-07-10 17:25:05 +0800

    1. add AutoSetTexture.java to set aspect ratio

---
+++ Camera2Lib/src/main/java/tw/com/hgdata/www/cameralib/AutoSetTextureView.java
@@ -0,0 +1,68 @@
+package tw.com.hgdata.www.cameralib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.TextureView;
+import
+
+import java.util.IllegalFormatException;
+
+/**
+ * Created by lackary on 2016/7/10.
+ */
+public class AutoSetTextureView extends TextureView {
+
+    private int ratioWidth;
+    private int ratioHeight;
+
+    public AutoSetTextureView(Context context) {
+        super(context);
+    }
+
+    public AutoSetTextureView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AutoSetTextureView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public AutoSetTextureView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    /***
+     * To  set aspect ration the view
+     * @param width
+     * @param height
+     */
+    public void SetAspectRatio(int width, int height) {
+
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Size cannot be negative.");
+        }
+
+        ratioWidth = width;
+        ratioHeight = height;
+
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = widthMeasureSpec;
+        int height = heightMeasureSpec;
+
+        if (widthMeasureSpec == 0 || heightMeasureSpec == 0) {
+            setMeasuredDimension(width, height);
+        } else  {
+            if (width < height * ratioWidth / ratioHeight) {
+                setMeasuredDimension(width, width * ratioHeight / ratioWidth);
+            } else {
+                setMeasuredDimension(height * ratioWidth / ratioHeight, height);
+            }
+        }
+    }
+}
