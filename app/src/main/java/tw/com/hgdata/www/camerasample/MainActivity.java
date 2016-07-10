ba6935d12bca40cc5f6b010ef9fbd24022347c51
lackary <lackar@gmail.com>
2016-07-10 16:19:51 +0800

    1. add Camera2 Lib package by Android 5.0 Camera2 API 2. test Camera2 Api like CameraCharacteristics camera id , CaptureRequest and key

--- app/src/main/java/tw/com/hgdata/www/camerasample/MainActivity.java
+++ app/src/main/java/tw/com/hgdata/www/camerasample/MainActivity.java
@@ -1,13 +1,45 @@
 package tw.com.hgdata.www.camerasample;
 
+import android.graphics.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        try {
+            for(String cameraId :  cameraManager.getCameraIdList()) {
+                Log.i(TAG, "Camera Id: " + cameraId);
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
+                List<CaptureRequest.Key<?>> captureRequestKeys = cameraCharacteristics.getAvailableCaptureRequestKeys();
+                Log.i(TAG, "captureRequestKeys" + captureRequestKeys.toString());
+                List<CaptureResult.Key<?>> captureResultKeys = cameraCharacteristics.getAvailableCaptureResultKeys();
+                Log.i(TAG, "captureResultKeys:" + captureResultKeys.toString());
+                List<CameraCharacteristics.Key<?>> cameraCharacteristicsKeys = cameraCharacteristics.getKeys();
+                Log.i(TAG, "cameraCharacteristicsKeys: " +cameraCharacteristicsKeys.toString());
+
+                int[] modes = cameraCharacteristics.get(CameraCharacteristics.COLOR_CORRECTION_AVAILABLE_ABERRATION_MODES);
+                for(int i : modes) {
+                    Log.i(TAG, "mode: " + i);
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
